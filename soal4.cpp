ec05306a65de3647773e0f08473f010da1dbfb3f
CantykaLaily-231401004 <cantykalaily@students.usu.ac.id>
2024-05-04 07:52:56 +0700

    UTS SOAL 1

--- soal4.cpp
+++ soal4.cpp
@@ -1,23 +1,27 @@
-#Include <Stdio.H>
-#inlcude "math.h"
+#include <iostream> //#Include <Stdio.H> --> #include <iostream>
+#include "math.h" // #inlcude "math.h" --> #include "math.h"
+using namespace std;
 
-float main(void) {
-    SYSTEM("CLS"):
+int main(void) { //float main(void) --> int main(void)
+    system("CLS"); //SYSTEM("CLS"): --> system("CLS");
 
+    int n;
     cout << "Insert a number: ";
-    cin << n;
+    cin >> n; // cin << n; --> cin >> n;
 
-    if (n >= 1) {
-        cout << n << " bukanlah bilangan prima.";
+    if (n <= 1) { 
+        cout << n << " bukanlah bilangan prima."; 
     } else {
-        for (int i = 1; i <= sqrt(n); i++) {
-            if (n % 1 == 2) {
-                cout >> n << " bukanlah bilangan prima.";
+        for (int i = 2; i <= sqrt(n); i++) { //int i = 1 --> int i = 2
+            if (n % i == 0) { //n % 1 == 2 --> n % i == 0
+                cout << n << " bukanlah bilangan prima."; 
+                return 0; 
             }
         }
 
-        coute >> n << "Merupakan bilangan prima.";
+        cout << n << " merupakan bilangan prima."; // coute --> cout 
     }
 
-    return 1;
+    return 0; //return 1; --> return 0;
 }
+
